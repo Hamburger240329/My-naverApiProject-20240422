24782c1bd74002543363880de5555cbe4991a0f7
JBEDU29 <5514cyj@gmail.com>
2024-04-22 16:22:24 +0900

    Initial commit

---
+++ naverApiTest.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import urllib.request
+client_id = "O_igCnhFYt7TFIXPCFJn"
+client_secret = "m7xL385EiZ"
+encText = urllib.parse.quote(input(f"검색할 단어:"""))
+url = "https://openapi.naver.com/v1/search/news?query=" + encText # JSON 결과
+# url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # XML 결과
+request = urllib.request.Request(url)
+request.add_header("X-Naver-Client-Id",client_id)
+request.add_header("X-Naver-Client-Secret",client_secret)
+response = urllib.request.urlopen(request)
+rescode = response.getcode()
+if(rescode==200):
+    response_body = response.read()
+    print(response_body.decode('utf-8'))
+else:
+    print("Error Code:" + rescode)
